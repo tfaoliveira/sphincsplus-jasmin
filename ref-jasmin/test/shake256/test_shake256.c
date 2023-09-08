09a2732d71522cea67a03a42480fcef3cd5dee27
Tiago Oliveira <tfaoliveira@gmail.com>
2023-09-08 15:41:33 +0200

    ref-jasmin/test: add macros.h common header to avoid using sed to generate code; expects that JASMINPP supports --param option to set param value in command line

--- ref-jasmin/test/shake256/test_shake256.c
+++ ref-jasmin/test/shake256/test_shake256.c
@@ -3,11 +3,10 @@
 #include <string.h>
 #include <assert.h>
 
+#include "macros.h"
+
 #include "print.c"
 #include "notrandombytes.c"
-
-extern void test_shake256(uint8_t *out, const uint8_t *in); 
-extern void shake256(uint8_t *output, size_t outlen, const uint8_t *input, size_t inlen); // from fips202.c
 
 #ifndef OUTLEN 
 #define OUTLEN 32
@@ -21,6 +20,12 @@ extern void shake256(uint8_t *output, size_t outlen, const uint8_t *input, size_
 #define TESTS 100000
 #endif
 
+#define shake256_jazz NAMESPACE2(shake256, OUTLEN, INLEN)
+
+extern void shake256_jazz(uint8_t *out, const uint8_t *in);
+extern void shake256(uint8_t *output, size_t outlen, const uint8_t *input, size_t inlen); // from fips202.c
+
+
 int main()
 {
   uint8_t out0[OUTLEN], out1[OUTLEN];
@@ -33,7 +38,7 @@ int main()
     randombytes(in0, INLEN);
     memcpy(in1, in0, INLEN);
  
-    test_shake256(out0, in0);
+    shake256_jazz(out0, in0);
     shake256(out1, OUTLEN, in1, INLEN);
 
     assert(memcmp(out0, out1, 32) == 0);
