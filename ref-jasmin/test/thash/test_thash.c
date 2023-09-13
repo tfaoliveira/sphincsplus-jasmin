c6fd2303c2e0e6af26ee6cc78897eea4e5b154cf
Rui Fernandes <ruipedro16@protonmail.com>
2023-09-13 09:52:08 +0200

    ref-jasmin/test: Test thash (.c doesnt compile due to undefined reference to thash C impl)

--- ref-jasmin/test/thash/test_thash.c
+++ ref-jasmin/test/thash/test_thash.c
@@ -45,25 +45,51 @@ target function:
     reg ptr u8[SPX_N]
 */
 extern void thash_jazz(
-  uint8_t *out,
-  const uint8_t *in,
-  const uint8_t *pub_seed,
-  uint32_t addr[8]
-);
+    uint8_t *out,
+    const uint8_t *in,
+    const uint8_t *pub_seed,
+    uint32_t addr[8]);
 
 // implementation from, for instance, ../../thash_shake_robust.c / ../../thash_shake_simple.c
 extern void thash(
-  uint8_t *out,
-  const uint8_t *in,
-  unsigned int inblocks,
-  const spx_ctx *ctx,
-  uint32_t addr[8]
+    unsigned char *out, 
+    const unsigned char *in, 
+    unsigned int inblocks,
+    const spx_ctx *ctx, 
+    uint32_t addr[8]
 );
 
-int main()
+static spx_ctx init_ctx(void)
 {
-  // TODO: tests: for each test gen rnd in/pub_seed/addr, check out
+  spx_ctx ctx;
+  randombytes(ctx.pub_seed, SPX_N); // TODO: FIXME: This is probably wrong (?)
+  randombytes(ctx.sk_seed, SPX_N);  // TODO: FIXME: This is probably wrong (?)
+  return ctx;
+}
+
+int main(void)
+{
+  uint8_t out0[SPX_N], out1[SPX_N];
+  uint8_t in0[SPX_N * INBLOCKS], in1[SPX_N * INBLOCKS];
+  uint32_t addr[8];
+  spx_ctx ctx; // pub_seed is here
+
+  int t;
+  for (t = 0; t < TESTS; t++)
+  {
+    ctx = init_ctx();
+
+    randombytes(in0, SPX_N * INBLOCKS);
+    memcpy(in1, in0, SPX_N * INBLOCKS);
+
+    thash_jazz(out0, in0, ctx.pub_seed, addr);
+    memcpy(out1, out0, SPX_N); // TODO: Remove this. This is only here to prevent error from 
+    // thash(out1, in1, INBLOCKS, &ctx, addr); // undefined reference
+
+    assert(memcmp(out0, out1, SPX_N) == 0);
+  }
+
+  printf("PASS: thash = { inblocks : %d }\n", INBLOCKS);
 
   return 0;
 }
-
