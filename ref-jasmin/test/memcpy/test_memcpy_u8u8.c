ca4b22c74216f5741dc4506567f113802bd06262
Rui Fernandes <ruipedro16@protonmail.com>
2023-11-20 05:41:32 +0100

    fix

--- ref-jasmin/test/memcpy/test_memcpy_u8u8.c
+++ ref-jasmin/test/memcpy/test_memcpy_u8u8.c
@@ -22,39 +22,13 @@
 #endif
 
 #define memcpy_u8u8_jazz NAMESPACE2(x_memcpy_u8u8, OUTLEN, INLEN)
-#define memcpy_u8u8_i_jazz NAMESPACE2(x_memcpy_u8u8_i, OUTLEN, INLEN)
 #define memcpy_u8u8p_jazz NAMESPACE1(x_memcpy_u8u8p, OUTLEN)
 
 extern void memcpy_u8u8_jazz(uint8_t *out, uint64_t offset, const uint8_t *in);
-extern void memcpy_u8u8_i_jazz(uint8_t *out, uint64_t offset, const uint8_t *in);
 extern void memcpy_u8u8p_jazz(uint8_t *out, uint64_t offset, const uint8_t *in, uint64_t inlen);
-
-void test_x_memcpy_u8u8_i(void);
-
-void test_x_memcpy_u8u8_i(void) {
-  uint8_t out_ref[OUTLEN], out_jazz[OUTLEN];
-  uint8_t in_ref[INLEN], in_jazz[INLEN];
-
-  for (int i = 0; i < TESTS; i++) {
-    for (size_t offset = 0; offset < INLEN; i++) {
-      randombytes(in_ref, INLEN);
-      memcpy(in_jazz, in_ref, INLEN);
-
-      memset(out_ref, 0, OUTLEN);
-      memset(out_jazz, 0, OUTLEN);
-
-      memcpy(out_ref, in_ref + offset, offset);
-      memcpy_u8u8_i_jazz(out_jazz, offset, in_jazz);
-      assert(memcmp(out_jazz, out_ref, offset) == 0);
-    }
-  }
-}
 
 int main()
 {
-  test_x_memcpy_u8u8_i();
-
-
   uint8_t out0[OUTLEN], out1[OUTLEN], out2[OUTLEN];
   uint8_t in0[INLEN], in1[INLEN], in2[INLEN];
   uint64_t offset;
