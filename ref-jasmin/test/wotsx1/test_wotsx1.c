411563b93664ed5ae9e331a81d8af927cb2ff120
Rui Fernandes <ruipedro16@protonmail.com>
2023-11-23 12:06:28 +0100

    tests for wotsx1

--- ref-jasmin/test/wotsx1/test_wotsx1.c
+++ ref-jasmin/test/wotsx1/test_wotsx1.c
@@ -1,13 +1,14 @@
 #include <assert.h>
 #include <inttypes.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "context.h"
 #include "macros.h"
 #include "notrandombytes.c"
 #include "print.c"
+#include "wotsx1.h"
 
 #ifndef HASH
 #define HASH shake
@@ -17,28 +18,14 @@
 #define PARAM 128f
 #endif
 
-#ifndef MSG_LEN
-#define MSG_LEN 64
-#endif
-
-
 #ifndef TESTS
 #define TESTS 100
 #endif
 
 extern void wots_gen_leafx1(unsigned char *dest, const spx_ctx *ctx, uint32_t leaf_idx,
                             void *v_info);
-// TODO: FIXME: Make leafidx a uint32_t instead of a pointer
 extern void wots_gen_leafx1_jazz(uint8_t *dest, const uint8_t *pub_seed, const uint8_t *sk_seed,
-                                 uint32_t *leaf_idx, struct leaf_info_x1 *info);
-
-// struct leaf_info_x1 {
-//     unsigned char *wots_sig; = 0
-//     uint32_t wots_sign_leaf; /* The index of the WOTS we're using to sign */
-//     uint32_t *wots_steps; u32[SPX_WOTS_LEN]
-//     uint32_t leaf_addr[8];
-//     uint32_t pk_addr[8];
-// };
+                                 uint32_t leaf_idx, struct leaf_info_x1 *info);
 
 static spx_ctx init_ctx(void) {
     spx_ctx ctx;
@@ -49,34 +36,39 @@ static spx_ctx init_ctx(void) {
 
 static struct leaf_info_x1 init_leaf_info(void) {
     struct leaf_info_x1 info;
-    randombytes((uint8_t*)info.wots_sig, sizeof(unsigned char));
-    randombytes((uint8_t*)info.wots_sign_leaf, sizeof(uint32_t));
-    randombytes((uint8_t*)info.wots_steps, sizeof(uint32_t) * SPX_WOTS_LEN);
-    randombytes((uint8_t*)info.leaf_addr, sizeof(uint32_t) * 8);
-    randombytes((uint8_t*)info.pk_addr, sizeof(uint32_t) * 8);
+    unsigned steps[SPX_WOTS_LEN] = {0};
+    uint32_t addr[8];
+
+    randombytes((uint8_t *)addr, sizeof(uint32_t) * 8);
+    info.wots_sig = 0;
+    info.wots_sign_leaf = ~0u;
+    info.wots_steps = steps;
+    memcpy(&info.leaf_addr[0], addr, 32);
+    memcpy(&info.pk_addr[0], addr, 32);
     return info;
 }
 
 int main(void) {
-    unsigned char dest0[SPX_N];
-    uint8_t dest1[SPX_N];
-    spx_ctx ctx;
+    unsigned char dest_ref[SPX_N];
+    uint8_t dest_jazz[SPX_N];
     uint32_t leaf_idx;
-    struct leaf_info_x1 info;
+    spx_ctx ctx;
+    struct leaf_info_x1 leaf_ref, leaf_jazz;
 
-    // Suppress compiler warniings
-    (void) dest0;
-    (void) dest1;
-    (void) leaf_idx;
-
-
-    for (int i = 0; i<TESTS;i++) {
+    for (int i = 0; i < TESTS; i++) {
         ctx = init_ctx();
-        info = init_leaf_info();
-        (void) ctx;
-        (void) info;
+        randombytes((uint8_t *)dest_ref, SPX_N * sizeof(unsigned char));
+        randombytes((uint8_t *)&leaf_idx, sizeof(uint32_t));
+        leaf_ref = init_leaf_info();
+        memcpy(&leaf_jazz, &leaf_ref, sizeof(struct leaf_info_x1));
 
+        wots_gen_leafx1(dest_ref, &ctx, leaf_idx, &leaf_ref);
+        wots_gen_leafx1_jazz(dest_jazz, ctx.pub_seed, ctx.sk_seed, leaf_idx, &leaf_jazz);
+
+        assert(memcmp(dest_ref, dest_jazz, SPX_N));
+        assert(memcmp(&leaf_ref, &leaf_jazz, sizeof(struct leaf_info_x1)));
     }
-    printf("Pass wotsx1\n");
+
+    puts("Pass wotsx1");
     return 0;
 }
