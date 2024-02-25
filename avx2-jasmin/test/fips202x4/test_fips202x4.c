953caab5206d275eece6b9868f3cdfed17e61a35
Rui Fernandes <ruipedro16@protonmail.com>
2024-02-25 20:32:11 +0100

    fix compilation error

--- avx2-jasmin/test/fips202x4/test_fips202x4.c
+++ avx2-jasmin/test/fips202x4/test_fips202x4.c
@@ -19,6 +19,10 @@
 #define INLEN 32
 #endif
 
+#ifndef OUTLEN
+#define OUTLEN 32
+#endif
+
 #ifndef MAX_BLOCKS
 #define MAX_BLOCKS 20
 #endif
@@ -28,22 +32,17 @@
 #define str(s) #s
 #define xstr(s) str(s)
 
-#define PASTE1(x, y) x##_##y
-#define NAMESPACE1(x, y) PASTE1(x, y)
-
 extern void KeccakF1600_StatePermute4x_jazz(__m256i state[25]);
 
 #define keccak_absorb4x_jazz NAMESPACE1(keccak_absorb4x_jazz, INLEN)
 extern void keccak_absorb4x_jazz(__m256i state[25], uint8_t *m0, uint8_t *m1, uint8_t *m2, uint8_t *m3);
 
-extern void shake256_squeezeblock4x_jazz(uint8_t *h0, uint8_t *h1, uint8_t *h2, uint8_t *h3, __m256i state[25]);
-extern void shake256_squeezeblocks_4x_jazz(uint8_t *h0, uint8_t *h1, uint8_t *h2, uint8_t *h3, uint64_t nblocks,
-                                           __m256i state[25]);
+#define shake256_squeezeblocks_4x_jazz NAMESPACE1(shake256_squeezeblocks_4x_jazz, OUTLEN)
+extern void shake256_squeezeblocks_4x_jazz(uint8_t *h0, uint8_t *h1, uint8_t *h2, uint8_t *h3, __m256i state[25]);
 
 void test_KeccakF1600_StatePermute4x(void);
 void test_keccak_absorb4x(void);
-void test_shake256_squeezeblock4x(void);
-void test_shake256_squeezeblocks4x(int nblocks);
+void test_shake256_squeezeblocks4x(void);
 
 void test_KeccakF1600_StatePermute4x(void) {
     bool debug = true;
@@ -95,7 +94,7 @@ void test_keccak_absorb4x(void) {
     }
 }
 
-void test_shake256_squeezeblock4x(void) {
+void test_shake256_squeezeblocks4x(void) {
     bool debug = true;
 
     __m256i state_ref[25];
@@ -106,57 +105,11 @@ void test_shake256_squeezeblock4x(void) {
     uint8_t h2_ref[SHAKE256_RATE], h2_jazz[SHAKE256_RATE];
     uint8_t h3_ref[SHAKE256_RATE], h3_jazz[SHAKE256_RATE];
 
-    for (int i = 0; i < TESTS; i++) {
-        if (debug) {
-            printf("shake256_squeezeblock4x_jazz]: Test %d/%d\n", i, TESTS);
-        }
-
-        randombytes((uint8_t *)state_ref, 25 * sizeof(__m256i));
-        memcpy(state_jazz, state_ref, 25 * sizeof(__m256i));
-
-        randombytes(h0_ref, SHAKE256_RATE);
-        memcpy(h0_jazz, h0_ref, SHAKE256_RATE);
-
-        randombytes(h1_ref, SHAKE256_RATE);
-        memcpy(h1_jazz, h1_ref, SHAKE256_RATE);
-
-        randombytes(h2_ref, SHAKE256_RATE);
-        memcpy(h2_jazz, h2_ref, SHAKE256_RATE);
-
-        randombytes(h3_ref, SHAKE256_RATE);
-        memcpy(h3_jazz, h3_ref, SHAKE256_RATE);
-
-        assert(memcmp(h0_jazz, h0_ref, SHAKE256_RATE) == 0);
-        assert(memcmp(h1_jazz, h1_ref, SHAKE256_RATE) == 0);
-        assert(memcmp(h2_jazz, h2_ref, SHAKE256_RATE) == 0);
-        assert(memcmp(h3_jazz, h3_ref, SHAKE256_RATE) == 0);
-        assert(memcmp(state_jazz, state_ref, 25 * sizeof(__m256i)) == 0);
-
-        keccak_squeezeblocks4x(h0_ref, h1_ref, h2_ref, h3_ref, 1, state_ref, SHAKE256_RATE);
-        shake256_squeezeblock4x_jazz(h0_jazz, h1_jazz, h2_jazz, h3_jazz, state_jazz);
-
-        assert(memcmp(h0_jazz, h0_ref, SHAKE256_RATE) == 0);
-        assert(memcmp(h1_jazz, h1_ref, SHAKE256_RATE) == 0);
-        assert(memcmp(h2_jazz, h2_ref, SHAKE256_RATE) == 0);
-        assert(memcmp(h3_jazz, h3_ref, SHAKE256_RATE) == 0);
-        assert(memcmp(state_jazz, state_ref, 25 * sizeof(__m256i)) == 0);
-    }
-}
-
-void test_shake256_squeezeblocks4x(int nblocks) {
-    bool debug = true;
-
-    __m256i state_ref[25];
-    __m256i state_jazz[25];
-
-    uint8_t h0_ref[SHAKE256_RATE], h0_jazz[SHAKE256_RATE];
-    uint8_t h1_ref[SHAKE256_RATE], h1_jazz[SHAKE256_RATE];
-    uint8_t h2_ref[SHAKE256_RATE], h2_jazz[SHAKE256_RATE];
-    uint8_t h3_ref[SHAKE256_RATE], h3_jazz[SHAKE256_RATE];
+    unsigned long long int nblocks = OUTLEN / SHAKE256_RATE;
 
     for (int i = 0; i < TESTS; i++) {
         if (debug) {
-            printf("[shake256_squeezeblock4x_jazz (%d blocks)]: Test %d/%d\n", nblocks, i, TESTS);
+            printf("[shake256_squeezeblock4x_jazz (%lld blocks)]: Test %d/%d\n", nblocks, i, TESTS);
         }
 
         randombytes((uint8_t *)state_ref, 25 * sizeof(__m256i));
@@ -181,7 +134,7 @@ void test_shake256_squeezeblocks4x(int nblocks) {
         assert(memcmp(state_jazz, state_ref, 25 * sizeof(__m256i)) == 0);
 
         keccak_squeezeblocks4x(h0_ref, h1_ref, h2_ref, h3_ref, nblocks, state_ref, SHAKE256_RATE);
-        shake256_squeezeblocks_4x_jazz(h0_jazz, h1_jazz, h2_jazz, h3_jazz, nblocks, state_jazz);
+        shake256_squeezeblocks_4x_jazz(h0_jazz, h1_jazz, h2_jazz, h3_jazz, state_jazz);
 
         assert(memcmp(h0_jazz, h0_ref, SHAKE256_RATE) == 0);
         assert(memcmp(h1_jazz, h1_ref, SHAKE256_RATE) == 0);
@@ -198,11 +151,8 @@ int main(void) {
     // Test absorb
     test_keccak_absorb4x();
 
-    test_shake256_squeezeblock4x();
-
-    // for (int i = 1; i <= MAX_BLOCKS; i++) {
-    //     test_shake256_squeezeblocks4x(i);
-    // }
+    // Test squeeze (the number of blocks to absorb is given by NBLOCKS = OUTLEN / SHAKE256_RATE)
+    test_shake256_squeezeblocks4x();
 
     printf("Pass fips202_4x { INLEN : %s ; OUTLEN : %s }\n", xstr(INLEN), xstr(OUTLEN));
     return 0;
--- shake-avx2/fips202x4.c
+++ shake-avx2/fips202x4.c
@@ -31,7 +31,7 @@ static void store64(uint8_t *x, uint64_t u) {
 extern void KeccakP1600times4_PermuteAll_24rounds(__m256i *s);
 #define KeccakF1600_StatePermute4x KeccakP1600times4_PermuteAll_24rounds
 
-static void keccak_absorb4x(__m256i *s, unsigned int r, const unsigned char *m0, const unsigned char *m1,
+void keccak_absorb4x(__m256i *s, unsigned int r, const unsigned char *m0, const unsigned char *m1,
                             const unsigned char *m2, const unsigned char *m3, unsigned long long int mlen,
                             unsigned char p) {
     unsigned long long i;
